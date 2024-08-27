9cfa42df6fe95d2530a081101fb94725616cb6bc
Khitiara <robotbrainify@gmail.com>
2024-08-27 14:02:46 -0400

    aa

--- Program.cs
+++ Program.cs
@@ -14,19 +14,30 @@ while (true) {
     ReadOnlySpan<char> cmd = split == -1 ? command : command.AsSpan(..split);
     switch (cmd) {
         case "x":
+        case "exit":
+        case "q":
+        case "quit":
             return;
         case "r":
+        case "roll":
+        case "c":
+        case "calc":
             if (split == -1) {
-                AnsiConsole.WriteLine("Cannot roll nothing");
+                AnsiConsole.MarkupLine("[yellow]Cannot roll nothing[/]");
                 continue;
             }
+
             string rest = command[(split + 1)..];
             DiceExpr result = DiceExpr.ParseAndRoll(Random.Shared, rest);
             stringBuilder.Clear();
+            stringBuilder.Append("Rolled ");
             result.PrintRoot(stringBuilder);
             stringBuilder.AppendLine();
             stringBuilder.AppendLine($" = {result.TotalValue}");
             AnsiConsole.Markup(stringBuilder.ToString());
+            break;
+        default:
+            AnsiConsole.MarkupLineInterpolated($"[red]Unknown command:[/] \"{command}\"");
             break;
     }
 }
